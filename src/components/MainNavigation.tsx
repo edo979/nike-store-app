e4d950a7dba833f5de61781bc3bd8f7639d1e8a0
edo979 <prezentabilan@gmail.com>
2022-10-07 18:42:14 +0200

    navigation finish

--- src/components/MainNavigation.tsx
+++ src/components/MainNavigation.tsx
@@ -9,19 +9,19 @@ export function MainNavigation() {
     <Navbar expand="sm" sticky={scrollPosition > 30 ? 'top' : undefined}>
       <Container>
         <Navbar.Brand href="#home">
-          <img src={logo} width={60} style={{ filter: 'brightness(0)' }} />
+          <img src={logo} width={60} />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbar" />
         <Navbar.Collapse id="navbar">
           <Nav className="me-auto"></Nav>
 
-          <Button variant="link" size="sm">
+          <Button>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="22"
               height="22"
               viewBox="0 0 512 512"
-              color="#000000"
+              fill="currentColor"
             >
               <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352c79.5 0 144-64.5 144-144s-64.5-144-144-144S64 128.5 64 208s64.5 144 144 144z" />
             </svg>
