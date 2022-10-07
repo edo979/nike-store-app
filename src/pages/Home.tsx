5ce01f30e4edd5ec14cb5b42f7a57f21c8ad7430
edo979 <prezentabilan@gmail.com>
2022-10-07 21:19:12 +0200

    fix header button, image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,12 +7,16 @@ export function Home() {
       <header>
         <MainNavigation />
 
-        <Container className="mt-5">
-          <Stack gap={3} className="col-sm-8 mx-auto">
-            <h1 className="text-bg-primary text-center ">
+        <Container className="mt-5 pb-5">
+          <Stack gap={5} className="col-sm-8 mx-auto">
+            <h1 className="text-bg-primary text-center display-4">
               Play With Electric Nike Adapt 2.0 Sneakers
             </h1>
-            <Button variant="light" size="lg">
+            <Button
+              variant="light"
+              size="lg"
+              className="mx-auto shadow z-index-3"
+            >
               Explore product
             </Button>
             <Image src="/assets/hero.png" />
