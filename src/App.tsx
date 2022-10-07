6aeb0d90095ac63beed61aef7c8a53f7bb590d45
edo979 <prezentabilan@gmail.com>
2022-10-07 09:48:54 +0200

    add and style icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
 import { MainNavigation } from './components/MainNavigation'
 import { Home } from './pages/Home'
 import './style/style.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <MainNavigation />
--- src/components/MainNavigation.tsx
+++ src/components/MainNavigation.tsx
@@ -3,7 +3,7 @@ import logo from '../assets/logo.png'
 
 export function MainNavigation() {
   return (
-    <Navbar bg="light" color="primary" expand="sm">
+    <Navbar bg="light" color="dark" expand="sm" className="shadow-sm">
       <Container>
         <Navbar.Brand href="#home">
           <img src={logo} width={60} style={{ filter: 'brightness(0)' }} />
@@ -12,14 +12,13 @@ export function MainNavigation() {
         <Navbar.Collapse id="navbar">
           <Nav className="me-auto"></Nav>
 
-          <Button
-            style={{ width: '3rem', height: '3rem' }}
-            variant="outline-secondary"
-          >
+          <Button variant="link" size="sm">
             <svg
               xmlns="http://www.w3.org/2000/svg"
+              width="22"
+              height="22"
               viewBox="0 0 512 512"
-              fill="currentColor"
+              color="#000000"
             >
               <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352c79.5 0 144-64.5 144-144s-64.5-144-144-144S64 128.5 64 208s64.5 144 144 144z" />
             </svg>
