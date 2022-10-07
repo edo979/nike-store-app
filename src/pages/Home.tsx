4d656790356b4386025611d690de21b583df2c39
edo979 <prezentabilan@gmail.com>
2022-10-07 22:56:57 +0200

    basic card layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,12 @@
-import { Button, Col, Container, Image, Row, Stack } from 'react-bootstrap'
+import {
+  Button,
+  Card,
+  Col,
+  Container,
+  Image,
+  Row,
+  Stack,
+} from 'react-bootstrap'
 import { MainNavigation } from '../components/MainNavigation'
 
 export function Home() {
@@ -24,6 +32,45 @@ export function Home() {
           </Stack>
         </Container>
       </header>
+
+      <main>
+        <Container>
+          <Row className="my-4">
+            <Col>
+              <h2>Popular Sales</h2>
+            </Col>
+          </Row>
+          <Row xs={1} sm={2} lg={3}>
+            <Col>
+              <Card className="bg-primary text-white">
+                <Row>
+                  <Col xs={6} style={{ zIndex: '1' }}>
+                    <Card.Body>
+                      <Card.Title className="mb-0">
+                        <h3 className="fs-6 text-nowrap mb-0">
+                          Nike Addapt BB 2.0
+                        </h3>
+                      </Card.Title>
+                      <Card.Text className="fs-6 text-nowrap">
+                        MEN Running Shoes
+                      </Card.Text>
+                    </Card.Body>
+                  </Col>
+                  <Col xs={6}>
+                    <Card.Img
+                      src="/assets/nike-adapt-bb.png"
+                      alt="nike adapt bb"
+                      style={{ rotate: '-30deg' }}
+                    />
+                  </Col>
+                </Row>
+              </Card>
+            </Col>
+            <Col>2</Col>
+            <Col>3</Col>
+          </Row>
+        </Container>
+      </main>
     </>
   )
 }
