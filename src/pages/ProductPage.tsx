7456991ce793ff26cc0d7c8643faa25ee563e30c
edo979 <prezentabilan@gmail.com>
2022-10-12 22:51:00 +0200

    finish product page

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -6,28 +6,12 @@ import { shoes } from '../data/shoes'
 export function ProductPage() {
   const { id } = useParams()
   const product = shoes.find((i) => i.id === id)
-  const { openCart, addItem } = useCart()
+  const { addItem } = useCart()
   const navigate = useNavigate()
   if (product === undefined) return null
 
   return (
     <main className="product container">
-      <button
-        className="btn btn-outline-secondary d-flex gap-2 align-items-center"
-        onClick={() => navigate(-1)}
-        style={{ marginTop: '100px' }}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 448 512"
-          fill="currentColor"
-          height={18}
-        >
-          <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
-        </svg>
-        Back
-      </button>
-
       <div
         className="position-absolute w-100 d-block, bg-primary"
         style={{
@@ -37,7 +21,7 @@ export function ProductPage() {
         }}
       ></div>
 
-      <div className=" col-xxl-8 px-4 py-4">
+      <div className="px-4 py-4 mt-5">
         <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
           <div className="col-10 col-sm-8 col-lg-6 mx-auto">
             <img
@@ -54,29 +38,22 @@ export function ProductPage() {
             <p className="lead">{product.text}</p>
             <div className="d-grid gap-2 d-md-flex justify-content-md-start">
               <button
-                type="button"
-                className="btn btn-primary btn-lg px-4 me-md-2"
-                onClick={openCart}
+                className="btn btn-outline-secondary d-flex gap-2 align-items-center"
+                onClick={() => navigate(-1)}
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
+                  viewBox="0 0 448 512"
                   fill="currentColor"
-                  aria-hidden="true"
-                  width={24}
-                  height={24}
+                  height={18}
                 >
-                  <path
-                    fillRule="evenodd"
-                    d="M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 004.25 22.5h15.5a1.875 1.875 0 001.865-2.071l-1.263-12a1.875 1.875 0 00-1.865-1.679H16.5V6a4.5 4.5 0 10-9 0zM12 3a3 3 0 00-3 3v.75h6V6a3 3 0 00-3-3zm-3 8.25a3 3 0 106 0v-.75a.75.75 0 011.5 0v.75a4.5 4.5 0 11-9 0v-.75a.75.75 0 011.5 0v.75z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>{' '}
-                Open Cart
+                  <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
+                </svg>
+                Back
               </button>
               <button
                 type="button"
-                className="btn btn-outline-secondary btn-lg px-4"
+                className="btn btn-primary btn-lg px-4"
                 onClick={() => addItem(product.id)}
               >
                 Buy Now
@@ -86,13 +63,13 @@ export function ProductPage() {
         </div>
       </div>
 
-      <div className="row my-2 mt-3">
+      <div className="row my-2">
         <div className="col">
           <h2>Popular Sales</h2>
         </div>
       </div>
 
-      <div className="pb-4 row row-cols-xs-1 row-cols-sm-2 row-cols-lg-3 g-4 g-sm-3 g-md-4 ">
+      <div className="pb-5 row row-cols-xs-1 row-cols-sm-2 row-cols-lg-3 g-4 g-sm-3 g-md-4 ">
         <CardV1List />
       </div>
     </main>
